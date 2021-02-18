2cdfdecdcd14e7be56d90ac4a2e5809cff7defc6
ek <ek@artificialcore.com>
2021-02-18 19:58:16 +0200

    initial commit https://docs.microsoft.com/en-us/dotnet/framework/network-programming/asynchronous-client-socket-example

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace msdn_async_client_socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
